a575539adf49e8b81e034936b35ae83b59a0c590
JeremyCPE <mohariry.jeremy@gmail.com>
2024-12-15 16:00:37 +0100

    Enhance Hero component with social links and update icons for consistency

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Play } from 'lucide-react';
+import {socialLinks} from '../Data/socialLinks';
+
 
 function Hero() {
   return (
-    <div className="relative h-screen flex items-center justify-center">
+    <div className="relative h-screen flex items-center justify-center bg-black z-10">
       <div className="absolute inset-0">
         <img
           src="https://cdn.prod.website-files.com/66af176a9ccda9ebdecbb5a3/66af3bad5a631306c413353f_a_whyte_l.jpg"
@@ -19,6 +21,22 @@ function Hero() {
             <Play className="w-5 h-5" />
             Buy now
           </button>
+        </div>
+      </div>
+      <div className='hidden md:flex px-4' >
+        <div className="grid gap-4">
+          {socialLinks.map((link, index) => (
+            <a
+              key={link.name}
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="pl-2 transform transition-all duration-300 hover:scale-110"
+            >
+                <div className="text-white-400">{link.icon}</div>
+            </a>
+          ))}
+          
         </div>
       </div>
     </div>
--- src/Data/socialLinks.tsx
+++ src/Data/socialLinks.tsx
@@ -5,19 +5,19 @@ import { SiSoundcloud, SiApplemusic } from "@icons-pack/react-simple-icons";
 export const socialLinks = [
     {
       name: 'Spotify',
-      icon: <Music className="w-8 h-8 animate-wiggle" style={{transform: 'rotate(5deg)'}} />,
+      icon: <Music className="w-8 h-8"/>,
       url: 'https://open.spotify.com/intl-fr/artist/7DZtdN4x13Amjw87cjdffb?dl_branch=1&nd=1&dlsi=cbc67d6397ed4a72',
       color: 'text-green-600 hover:text-green-700'
     },
     {
       name: 'Instagram',
-      icon: <Instagram className="w-8 h-8 animate-wiggle" style={{transform: 'rotate(5deg)'}} />,
+      icon: <Instagram className="w-8 h-8" />,
       url: 'https://instagram.com',
       color: 'text-pink-600 hover:text-pink-700'
     },
     {
       name: 'X',
-      icon: <Twitter className="w-8 h-8 animate-wiggle"  style={{transform: 'rotate(5deg)'}}/>,
+      icon: <Twitter className="w-8 h-8"/>,
       url: 'https://twitter.com',
       color: 'text-blue-600 hover:text-blue-700'
     },
@@ -29,25 +29,25 @@ export const socialLinks = [
     },
     {
       name: 'Facebook',
-      icon: <Facebook className="w-8 h-8 animate-wiggle" style={{transform: 'rotate(5deg)'}}/>,
+      icon: <Facebook className="w-8 h-8"/>,
       url: 'https://facebook.com',
       color: 'text-blue-600 hover:text-blue-700'
     },
     {
       name: 'Mail',
-      icon: <Mail className="w-8 h-8 animate-wiggle" style={{transform: 'rotate(5deg)'}}/>,
+      icon: <Mail className="w-8 h-8"/>,
       url: 'test@gmail.com',
       color: 'text-white-600 hover:text-white-700'
     },
     {
       name: 'AppleMusic',
-      icon: <SiApplemusic className="w-8 h-8 animate-wiggle" style={{transform: 'rotate(5deg)'}}/>,
+      icon: <SiApplemusic className="w-8 h-8"/>,
       url: 'https://music.apple.com/cn/artist/whyte/1471059668?l=en-GB',
       color: 'text-[#FA243C] hover:text-[#FA243C]'
     },
     {
       name: 'Soundclound',
-      icon: <SiSoundcloud className="w-8 h-8 animate-wiggle" style={{transform: 'rotate(5deg)'}}/>,
+      icon: <SiSoundcloud className="w-8 h-8"/>,
       url: 'https://soundcloud.com/whytetete',
       color: 'text-[#FF5500] hover:text-[#FF5500]'
     }
